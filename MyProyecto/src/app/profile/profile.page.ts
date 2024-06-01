df2a27ce7e36f167972005d3cdaea552808893cd
segajardor <se.gajardor@duocuc.cl>
2024-06-01 15:03:26 -0400

    perfil

--- MyProyecto/src/app/profile/profile.page.ts
+++ MyProyecto/src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  usernameRecibido: string='';
+  nameRecibido: string='';
+  lastnameRecibido: string='';
+  emailRecibido: string='';
+  countryRecibido: string='';
+  addressRecibido: string='';
+
+  constructor(private router: Router,
+              private activateroute: ActivatedRoute) {
+                this.activateroute.queryParams.subscribe(params => {
+                  
+                  if (this.router.getCurrentNavigation()?.extras?.state) {
+
+                    this.usernameRecibido = this.router.getCurrentNavigation()?.extras?.state?.['usernameEnviado'];
+                    this.nameRecibido = this.router.getCurrentNavigation()?.extras?.state?.['nameEnviado'];
+                    this.lastnameRecibido = this.router.getCurrentNavigation()?.extras?.state?.['lastnameEnviado'];
+                    this.emailRecibido = this.router.getCurrentNavigation()?.extras?.state?.['emailEnviado'];
+                    this.countryRecibido = this.router.getCurrentNavigation()?.extras?.state?.['countryEnviado'];
+                    this.addressRecibido = this.router.getCurrentNavigation()?.extras?.state?.['addressEnviado'];
+                  }
+                })
+}
 
   ngOnInit() {
   }
