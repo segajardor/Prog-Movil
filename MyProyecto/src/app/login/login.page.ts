cdc480bc9faf46ab8241325667c27e066bd7dadf
segajardor <se.gajardor@duocuc.cl>
2024-06-01 13:02:14 -0400

    Config login

--- MyProyecto/src/app/login/login.page.ts
+++ MyProyecto/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string='';
+  password: string='';
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    if (this.username === 'seba' && this.password === '123456') {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Credenciales incorrectas')
+    }
   }
 
 }
