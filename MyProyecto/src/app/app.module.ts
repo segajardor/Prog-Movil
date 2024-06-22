f8d7f3037971891020d6c8ce5f35e95572abfe6b
segajardor <se.gajardor@duocuc.cl>
2024-06-22 18:37:24 -0400

    cambios

--- MyProyecto/src/app/app.module.ts
+++ MyProyecto/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- MyProyecto/src/app/breeds/breeds.page.ts
+++ MyProyecto/src/app/breeds/breeds.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreedsPage implements OnInit {
 
-  constructor() { }
+  breedsData: any[] = [];
+
+  datosUnidad: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.fetchBreedsData();
+  }
+
+  fetchBreedsData() {
+    this.http.get('https://api.thedogapi.com/v1/breeds').subscribe((response: any) => {
+      console.log(response);
+      
+    });
   }
 
 }
