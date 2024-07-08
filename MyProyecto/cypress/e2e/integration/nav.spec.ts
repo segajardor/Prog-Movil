7d3779e252d1288cd1fbb4c9f620712fde4259c6
segajardor <se.gajardor@duocuc.cl>
2024-07-08 11:17:06 -0400

    Prueba ingresar

--- MyProyecto/cypress/e2e/integration/nav.spec.ts
+++ MyProyecto/cypress/e2e/integration/nav.spec.ts
@@ -1,5 +1,7 @@
 describe('Navigation Tests', () => {
     it('should navigate to the about page', () => {
         cy.visit('http://localhost:8100/login');
+
+        cy.get('ion-button').contains('INGRESAR').click();
     });
 });
